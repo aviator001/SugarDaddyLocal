8279c003bcc4242111d7b53a7295514c9140eae8
Unknown <g@txt.am>
2019-06-30 06:27:07 -0700

    rewr

--- js/index.js
+++ js/index.js
@@ -42,5 +42,9 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
-
+function handleOpenURL(url) {
+  setTimeout(function() {
+    alert("received url: " + url);
+  }, 0);
+}
 app.initialize();

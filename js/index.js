40dfc3119fac2cd2cf726dac78217b5176572cb2
Unknown <g@txt.am>
2019-07-01 16:09:18 -0700

    rte

--- js/index.js
+++ js/index.js
@@ -42,9 +42,4 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
-function handleOpenURL(url) {
-  setTimeout(function() {
-    alert("received url: " + url);
-  }, 0);
-}
 app.initialize();
